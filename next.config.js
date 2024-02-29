71de3eacca8567f776ffb4a583b2aa471e965760
Rongbin99 <rongbingu999@gmail.com>
2024-02-29 10:10:28 -0500

    pls build

--- next.config.js
+++ next.config.js
@@ -1,15 +1,13 @@
 const withNextra = require("nextra")({
   theme: "nextra-theme-blog",
   themeConfig: "./theme.config.js",
+  unstable_staticImage: true,
   // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true,
-  },
   // any configs you need
 };
 
