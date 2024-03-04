553f624d978554fdcfce2b7aed4024b8bbf7ea3e
Rongbin <rongbingu999@gmail.com>
2024-03-04 06:24:29 -0500

    todo

---
+++ pages/404.js
@@ -0,0 +1,19 @@
+import { Helmet } from 'react-helmet';
+
+const notfound = () => {
+    return (
+      <div className="flex flex-col min-h-screen">
+        <Helmet>
+          <title>Error 404</title>
+        </Helmet>
+        <div className="flex flex-col items-center justify-center flex-grow">
+          <div className="text-center text-white">
+            <h1 className="text-2xl md:text-4xl mb-2">Error 404: Page Not Found</h1>
+            <p className="text-xs md:text-sm mb-4">ermmm...</p>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default notfound;
