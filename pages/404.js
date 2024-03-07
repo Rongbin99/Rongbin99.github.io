18302da0630a3647a8100ae65cb2cdfe918da453
Rongbin99 <rongbingu999@gmail.com>
2024-03-07 18:46:00 -0500

    no tsParticles lul

--- pages/404.js
+++ pages/404.js
@@ -1,19 +1,20 @@
 import { Helmet } from 'react-helmet';
 
-const notfound = () => {
-    return (
-      <div className="flex flex-col min-h-screen">
-        <Helmet>
-          <title>Error 404</title>
-        </Helmet>
-        <div className="flex flex-col items-center justify-center flex-grow">
-          <div className="text-center text-white">
-            <h1 className="text-2xl md:text-4xl mb-2">Error 404: Page Not Found</h1>
-            <p className="text-xs md:text-sm mb-4">ermmm...</p>
-          </div>
+const NotFound = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Helmet>
+        <title>Error 404</title>
+      </Helmet>
+
+      <div className="flex flex-col items-center justify-center flex-grow">
+        <div className="text-center text-white">
+          <h1 className="text-2xl md:text-4xl mb-2">Error 404: Page Not Found</h1>
+          <p className="text-xs md:text-sm mb-4">erm...</p>
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
   
-  export default notfound;
+export default NotFound;
