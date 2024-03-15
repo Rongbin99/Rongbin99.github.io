80eb71258a3adbdd7bdb87ffcfa776c3053391f3
Rongbin Gu <81097533+Rongbin99@users.noreply.github.com>
2024-03-15 14:45:29 -0400

    force dark mode

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,7 @@ export default function Document() {
   };
 
   return (
-    <Html lang="en">
+    <Html lang="en" class="dark">
       <Head>
         <meta name="robots" content="follow, index" />
         <meta name="description" content={meta.description} />
