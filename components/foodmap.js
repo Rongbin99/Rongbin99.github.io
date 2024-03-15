bca82c760496b312c02ec949713a540ec7395040
Rongbin99 <rongbingu999@gmail.com>
2024-03-15 13:42:41 -0400

    foodmap embed done

--- components/foodmap.js
+++ components/foodmap.js
@@ -1,9 +1,7 @@
-
-
 const foodmap = () => {
     return (
-        <div>
-            <p>Work in progress...</p>
+        <div className="flex items-center justify-center -mb-16 -mt-3">
+            <iframe src="https://www.google.com/maps/d/embed?mid=1zw_JcZMjucS9WACQpN2JaW4AseXkoas&ehbc=2E312F" width={"100%"} height={600} style={{border: 0}}></iframe>
         </div>
     )
 }
