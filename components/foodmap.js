302b355a1536430c11634954f40af1e3b673f1ff
Rongbin Gu <81097533+Rongbin99@users.noreply.github.com>
2025-02-12 02:29:04 -0500

    Fix #18 and #19 and Add PawLog Page (#20)
    This PR introduces a new Side Project post for PawLog from DeltaHacks XI
    back in Jan 2025.
    
    This PR fixes:
    - #18 
    - #19 
    caused by #14 
    
    Commits:
    - npm audit fix
    - Update Show Ratings
    - Restore Adobe Icons #19 
    - Fix Issue #18 
    - Add PawLog page

--- components/foodmap.js
+++ components/foodmap.js
@@ -1,6 +1,6 @@
 const foodmap = () => {
     return (
-        <div className="flex items-center justify-center -mb-16 mt-3">
+        <div className="flex items-center justify-center mt-3">
             <iframe src="https://www.google.com/maps/d/embed?mid=1zw_JcZMjucS9WACQpN2JaW4AseXkoas&ehbc=2E312F" width={"100%"} height={600} style={{border: 0}}></iframe>
         </div>
     )
